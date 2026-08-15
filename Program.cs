3607942bfdff98209ab2a319c62a996c5a8398c7
Vladymyr <vldyrex@gmail.com>
2026-08-15 13:18:43 +0300

    Add initial Avalonia application

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using System;
+
+namespace WebBoost;
+
+sealed class Program
+{
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UseWin32()
+            .UseSkia()
+            .UseHarfBuzz()
+#if DEBUG
+            .WithDeveloperTools()
+#endif
+            .WithInterFont()
+            .LogToTrace();
+}
